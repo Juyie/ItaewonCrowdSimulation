df4a8a9b96dc564ec1a939e0057da799d3f6c739
Juyie Hwang <juyie433@gmail.com>
2023-02-03 20:07:44 +0900

    add count and display agent number UI

---
+++ Assets/Scripts/DisplayAgentNumber.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayAgentNumber : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] groups;
+
+    [SerializeField]
+    private Text uiText;
+
+    private int agentNumber = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject group in groups)
+        {
+            agentNumber += group.transform.childCount;
+        }
+        uiText.text = agentNumber.ToString();
+        agentNumber = 0;
+    }
+}
