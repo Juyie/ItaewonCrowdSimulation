c1c579af28a5763d80209359bda900d89821cc75
Juyie Hwang <ghkdwndl1004@ewhain.net>
2023-02-23 14:58:04 +0900

    feat: change scripts to entity physics

---
+++ Assets/Scripts/Physics/CalculateContactForcePhysics.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CalculateContactForcePhysics : MonoBehaviour
+{
+    public float forcePower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (transform.root.name != collision.transform.root.name)
+        {
+            forcePower = collision.impulse.magnitude / Time.fixedDeltaTime;
+        }
+        //Debug.Log("calculated force: " +forcePower);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (transform.root.name != collision.transform.root.name)
+        {
+            forcePower = collision.impulse.magnitude / Time.fixedDeltaTime;
+        }
+        //Debug.Log("calculated force: "  + forcePower);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        forcePower = 0;
+    }
+}
