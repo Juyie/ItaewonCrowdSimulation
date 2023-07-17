ce4fe5f0b241a236fa55ee2808b6059477508332
Juyie Hwang <ghkdwndl1004@ewhain.net>
2023-07-17 16:00:36 +0900

    update5

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Physics.Systems;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public bool Step = true;
+    public bool Exponential = false;
+    public float stepSize = 0.01f;
+    public float exponentialSize = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Step)
+        {
+            Exponential = false;
+        }
+        else
+        {
+            Exponential = true;
+        }
+    }
+}
