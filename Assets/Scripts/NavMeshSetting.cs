e2d6de1bb6e412361633002acafd6b07a33a0576
Juyie Hwang <ghkdwndl1004@ewhain.net>
2023-11-12 23:58:41 +0900

    Add: NavMeshSetting script

---
+++ Assets/Scripts/NavMeshSetting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshSetting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMesh.avoidancePredictionTime = 1.0f;
+        NavMesh.pathfindingIterationsPerFrame = 5000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
